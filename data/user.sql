fb749bcc9b080dc3d9f879fafe5eedfa283b8344
Gabriel Cossette <gabriel@gcossette.com>
2018-04-17 22:27:32 -0400

    Fix

--- data/user.sql
+++ data/user.sql
@@ -18,25 +18,11 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `pydio`
---
-
 use pydio;
-
-----------------------------------------------------------
-
-
-
---
--- Dumping data for table `ajxp_users`
---
 
 UPDATE `ajxp_users` SET password='sha256:1000:SH2I3qCiMqtpKQiiGf16I9GJrZM22q8I:Tib5ZnN0lZDV/DwCC7s0LfZhBxH/myhz' WHERE login='admin';
 
 FLUSH PRIVILEGES;
-
-----------------------------------------------------------
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
